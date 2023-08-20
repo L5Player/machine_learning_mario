4b09aeb580238304c77fef93401aa03366f8124d
l5player <l5player@163.com>
2023-08-21 07:59:33 +0800

    add new scripts

---
+++ new_run_test.bat
@@ -0,0 +1,6 @@
+set file="save\newtest"
+set inds=279
+
+@REM cmd /k "activate python37&&python -m retro.import ./rom&&python smb_ai_new.py --load-file %file% --load-inds %inds% --debug"
+cmd /k "activate python37&&python -m retro.import ./rom&&python smb_ai_new.py --load-file %file% --load-inds %inds% --no-display --debug"
+
